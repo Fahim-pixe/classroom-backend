df4b957ee46f65225cbb5ab0af3bed7420f24529
Md. Fahim Munem <fahimmunem87@gmail.com>
2026-08-04 03:17:35 +0600

    Added arcjet, apm and more

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,49 @@
-import  express  from  "express" ;
-import subjectsRouter from "./routes/subjects.ts";
+import('apminsight')
+  .then(({ default: AgentAPI }) => AgentAPI.config())
+  .catch(() => console.log('APM not available in this environment'));
+
 import cors from "cors";
+import express from "express";
+import { toNodeHandler } from "better-auth/node";
+
+import subjectsRouter from "./routes/subjects.ts";
+import usersRouter from "./routes/users.ts";
+import classesRouter from "./routes/classes.ts";
+import departmentsRouter from "./routes/departments.ts";
+import statsRouter from "./routes/stats.ts";
+import enrollmentsRouter from "./routes/enrollments.ts";
+
+// import securityMiddleware from "./middleware/security.ts";
+import { auth } from "./lib/auth.ts";
 
 const app = express();
 const PORT = 8000;
-if (!process.env.FRONTEND_URL) {
-    console.warn("FRONTEND_URL is not defined in the environment. Defaulting to http://localhost:5173");
-}
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // React app URL
+    methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+    credentials: true, // allow cookies
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
+// app.use(securityMiddleware);
+
 app.use("/api/subjects", subjectsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/classes", classesRouter);
+app.use("/api/departments", departmentsRouter);
+app.use("/api/stats", statsRouter);
+app.use("/api/enrollments", enrollmentsRouter);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Classroom API!");
+  res.send("Backend server is running!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
